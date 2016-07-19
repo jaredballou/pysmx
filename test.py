b69a664544358b8b5faaa20e51d01460409a8aa9
Jared Ballou <github@jballou.com>
2016-07-19 15:24:22 -0400

    Removed unwanted test script

--- test.py
+++
@@ -1,9 +0,0 @@
-import smx
-from pprint import pprint
-with open('/home/insserver/serverfiles/insurgency/addons/sourcemod/plugins/insurgency.smx', 'rb') as fp:
-    plugin = smx.SourcePawnPlugin(fp)
-#    print 'Loaded %s...' % plugin
-
-#pprint(plugin.Pubvar(plugin,0,'myinfo').value)
-print plugin.myinfo['version']
-#sp_file_hdr().version())
